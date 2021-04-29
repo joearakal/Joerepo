3fafaeb0f39ff24a63292c5aaf12e9e24fedd6f4
joearakal <joseph.arakal@nelnet.net>
2021-04-29 10:09:50 -0500

    commit2 sprint2

--- Sprint2/ex.js
+++ Sprint2/ex.js
@@ -1,41 +1,44 @@
 function addEntries() {
-    var newName = document.getElementById("myText").value ;
-    let newNum = document.getElementById("myNum").value ;
+    var newName = document.forms["myForm"]["myText"].value;
+    var newNum = document.forms["myForm"]["myNum"].value;
 
-    
     if (newName == "") {
-        alert ("Enter some text");
-    } else {
-        if (newNum == 1, 2 ) {
-            if (newNum==1){
-                var tableRef = document.getElementById("myTable1");
-                (tableRef.insertRow(tableRef.rows.length)).innerHTML = newText;}
-             else{
-                var tableRef = document.getElementById("myTable2");
-                (tableRef.insertRow(tableRef.rows.length)).innerHTML = newText;}
-            alert ("Added to List " + newNum);
-        } else {
-            alert ("Enter 1 or 2");
-        }
-       
- 
+        alert("Enter some text");
+        return false;
     }
-    document.getElementById("myText").value = "";
-    document.getElementById("myNum").value = "";
+    else if ((newNum != 1) && (newNum != 2)) {
+        alert("Enter 1 or 2");
+        document.forms["myForm"]["myNum"].value = "";
+        return false;
+    }
+
+    else if (newNum == 1) {
+        var tableRef = document.getElementById("table1");
+        (tableRef.insertRow(tableRef.rows.length)).innerHTML = newName;
+    }
+
+    else {
+        var tableRef = document.getElementById("table2");
+        (tableRef.insertRow(tableRef.rows.length)).innerHTML = newName;
+    }
+
+    document.forms["myForm"]["myText"].value = "";
+    document.forms["myForm"]["myNum"].value = "";
+
+    return true;
+    // we ont get to the next line, since control returned before it executes
+    alert("Added to List " + newNum);
 }
 
 
+
 function clearTable1() {
-    for(var i = 1;i<table1.rows.length;){
-        table1.deleteRow(i);
-    }
-    
-    alert ("List " + newNum + " cleared");
+    var tableRef = document.getElementById("table1");
+    // clears table1
+    tableRef.innerHTML = " ";
 }
-function clearTable1() {
-    
-    for(var i = 1;i<table2.rows.length;){
-        table2.deleteRow(i);
-    }
-    alert ("List " + newNum + " cleared");
+function clearTable2() {
+    var tableRef = document.getElementById("table2");
+    // clears table2
+    tableRef.innerHTML = " ";
 }
